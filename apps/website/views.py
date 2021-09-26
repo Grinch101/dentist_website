2163a48d42942b622336f195d635328f6b3a13a9
Grinch101 <masoud.kameli@gmail.com>
2021-09-26 18:28:49 -0400

    editing htmls jinja

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import plotly.graph_objects as go
 
+fig = go.Figure()
+fig.add_trace(go.Bar(x=[1,3,4],y=[2,3,1]))
+fig = fig.to_html(full_html=False)
 # Create your views here.
 def index(req):
-    return render(req,'home.html', {'A':'this is A'})
+    return render(req,'index.html', {'myFig':fig})
