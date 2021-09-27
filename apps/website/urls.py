81f802ecca26552141f7d7788c671bfa17e90b75
Grinch101 <masoud.kameli@gmail.com>
2021-09-27 00:33:23 -0400

    add renderer views and urls  in base.html

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -2,5 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name="Home Page"),
+    path('', views.index, name="index"),
+    path('tables/', views.tables, name="tables"),
+    path('register/', views.register, name="register"),
+    path('password/', views.password, name="password"),
+    path('login/', views.login, name="login"),
+    path('layout_static/', views.layout_static, name="layout_static"),
+    path('layout_sidenav_light/', views.layout_sidenav_light,
+         name="layout_sidenav_light"),
+    path('charts/', views.charts, name="charts")
 ]
--- apps/website/views.py
+++ apps/website/views.py
@@ -2,8 +2,37 @@ from django.shortcuts import render
 import plotly.graph_objects as go
 
 fig = go.Figure()
-fig.add_trace(go.Bar(x=[1,3,4],y=[2,3,1]))
+fig.add_trace(go.Bar(x=[1, 3, 4], y=[2, 3, 1]))
 fig = fig.to_html(full_html=False)
-# Create your views here.
+
+
 def index(req):
-    return render(req,'index.html', {'myFig':fig})
+    return render(req, 'index.html', {'myFig': fig})
+
+
+def tables(req):
+    return render(req, 'tables.html')
+
+
+def register(req):
+    return render(req, 'register.html')
+
+
+def password(req):
+    return render(req, 'password.html')
+
+
+def login(req):
+    return render(req, 'login.html')
+
+
+def layout_static(req):
+    return render(req, 'layout-static.html')
+
+
+def layout_sidenav_light(req):
+    return render(req, 'layout-sidenav-light.html')
+
+
+def charts(req):
+    return render(req, 'charts.html')
